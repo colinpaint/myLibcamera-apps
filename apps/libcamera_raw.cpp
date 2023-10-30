3a8b33617b8c9e8d9200a579d657a23754e0242a
colinpaint <88828347+colinpaint@users.noreply.github.com>
2023-10-30 11:45:33 +0000

    Update libcamera_raw.cpp

--- apps/libcamera_raw.cpp
+++ apps/libcamera_raw.cpp
@@ -13,7 +13,8 @@
 #include "encoder/null_encoder.hpp"
 #include "output/output.hpp"
 
-using namespace std::placeholders;
+using namespace std;
+using namespace placeholders;
 //}}}
 
 class LibcameraRaw : public LibcameraEncoder {
@@ -21,53 +22,60 @@ public:
   LibcameraRaw() : LibcameraEncoder() {}
 protected:
   // Force the use of "null" encoder.
-  void createEncoder() { encoder_ = std::unique_ptr<Encoder>(new NullEncoder (GetOptions())); }
+  void createEncoder() { encoder_ = unique_ptr<Encoder>(new NullEncoder (GetOptions())); }
   };
 
-//{{{
-// The main even loop for the application.
-static void event_loop (LibcameraRaw& app) {
+namespace {
+  //{{{
+  void event_loop (LibcameraRaw& app) {
 
-  VideoOptions const* options = app.GetOptions();
-  std::unique_ptr<Output> output = std::unique_ptr<Output>(Output::Create(options));
-  app.SetEncodeOutputReadyCallback (std::bind(&Output::OutputReady, output.get(), _1, _2, _3, _4));
-  app.SetMetadataReadyCallback (std::bind(&Output::MetadataReady, output.get(), _1));
+    VideoOptions const* options = app.GetOptions();
+    unique_ptr<Output> output = unique_ptr<Output>(Output::Create(options));
 
-  app.OpenCamera();
-  app.ConfigureVideo (LibcameraRaw::FLAG_VIDEO_RAW);
-  app.StartEncoder();
-  app.StartCamera();
-  auto start_time = std::chrono::high_resolution_clock::now();
+    app.SetEncodeOutputReadyCallback (bind (&Output::OutputReady, output.get(), _1, _2, _3, _4));
+    app.SetMetadataReadyCallback (bind (&Output::MetadataReady, output.get(), _1));
 
-  for (unsigned int count = 0; ; count++) {
-    LibcameraRaw::Msg msg = app.Wait();
+    app.OpenCamera();
+    app.ConfigureVideo (LibcameraRaw::FLAG_VIDEO_RAW);
+    app.StartEncoder();
+    app.StartCamera();
 
-    if (msg.type == LibcameraApp::MsgType::Timeout) {
-      LOG_ERROR ("ERROR: Device timeout detected, attempting a restart!!!");
-      app.StopCamera();
-      app.StartCamera();
-      continue;
+    auto start_time = chrono::high_resolution_clock::now();
+
+    for (unsigned int count = 0; ; count++) {
+      LibcameraRaw::Msg msg = app.Wait();
+
+      if (msg.type == LibcameraApp::MsgType::Timeout) {
+        LOG_ERROR ("ERROR: Device timeout detected, attempting a restart!!!");
+        app.StopCamera();
+        app.StartCamera();
+        continue;
+        }
+      if (msg.type != LibcameraRaw::MsgType::RequestComplete)
+        throw runtime_error ("unrecognised message!");
+
+      if (count == 0) {
+        libcamera::StreamConfiguration const &cfg = app.RawStream()->configuration();
+        LOG (1, "Raw stream: " << cfg.size.width << "x" 
+                               << cfg.size.height << " stride " 
+                               << cfg.stride << " format "
+                               << cfg.pixelFormat.toString());
+        }
+
+      LOG (2, "Viewfinder frame " << count);
+      auto now = chrono::high_resolution_clock::now();
+      if (options->timeout && (now - start_time) > options->timeout.value) {
+        app.StopCamera();
+        app.StopEncoder();
+        return;
+        }
+
+      app.EncodeBuffer (get<CompletedRequestPtr>(msg.payload), app.RawStream());
       }
-    if (msg.type != LibcameraRaw::MsgType::RequestComplete)
-      throw std::runtime_error ("unrecognised message!");
-    if (count == 0) {
-      libcamera::StreamConfiguration const &cfg = app.RawStream()->configuration();
-      LOG(1, "Raw stream: " << cfg.size.width << "x" << cfg.size.height << " stride " << cfg.stride << " format "
-                            << cfg.pixelFormat.toString());
-      }
-
-    LOG(2, "Viewfinder frame " << count);
-    auto now = std::chrono::high_resolution_clock::now();
-    if (options->timeout && (now - start_time) > options->timeout.value) {
-      app.StopCamera();
-      app.StopEncoder();
-      return;
-      }
-
-    app.EncodeBuffer (std::get<CompletedRequestPtr>(msg.payload), app.RawStream());
     }
+  //}}}
   }
-//}}}
+
 //{{{
 int main (int argc, char* argv[]) {
 
@@ -82,7 +90,7 @@ int main (int argc, char* argv[]) {
       event_loop (app);
       }
     }
-  catch (std::exception const &e) {
+  catch (exception const &e) {
     LOG_ERROR ("ERROR: *** " << e.what() << " ***");
     return -1;
     }
