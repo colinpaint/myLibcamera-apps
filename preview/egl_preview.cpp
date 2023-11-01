ce1069ff6a664695fc31c2e11727fff976dc8bf7
colinpaint <88828347+colinpaint@users.noreply.github.com>
2023-11-01 17:05:32 +0000

    fold

--- preview/egl_preview.cpp
+++ preview/egl_preview.cpp
@@ -69,10 +69,10 @@ private:
   };
   //}}}
 
-  void makeWindow(char const *name);
-  void makeBuffer(int fd, size_t size, StreamInfo const &info, Buffer &buffer);
+  void makeWindow (char const* name);
+  void makeBuffer (int fd, size_t size, StreamInfo const &info, Buffer &buffer);
 
-  ::Display *display_;
+  ::Display* display_;
   EGLDisplay egl_display_;
   Window window_;
   EGLContext egl_context_;
@@ -92,102 +92,162 @@ private:
 };
 //}}}
 
-//{{{
-static GLint compile_shader (GLenum target, const char* source) {
+namespace {
+  //{{{
+  GLint compile_shader (GLenum target, const char* source) {
 
-  GLuint s = glCreateShader (target);
-  glShaderSource (s, 1, (const GLchar**)&source, NULL);
-  glCompileShader (s);
+    GLuint s = glCreateShader (target);
+    glShaderSource (s, 1, (const GLchar**)&source, NULL);
+    glCompileShader (s);
 
-  GLint ok;
-  glGetShaderiv (s, GL_COMPILE_STATUS, &ok);
+    GLint ok;
+    glGetShaderiv (s, GL_COMPILE_STATUS, &ok);
 
-  if (!ok) {
-    GLchar* info;
-    GLint size;
+    if (!ok) {
+      GLchar* info;
+      GLint size;
 
-    glGetShaderiv (s, GL_INFO_LOG_LENGTH, &size);
-    info = (GLchar*)malloc(size);
+      glGetShaderiv (s, GL_INFO_LOG_LENGTH, &size);
+      info = (GLchar*)malloc(size);
 
-    glGetShaderInfoLog (s, size, NULL, info);
-    throw std::runtime_error ("failed to compile shader: " + std::string(info) + "\nsource:\n" + std::string (source));
-    }
-
-  return s;
-  }
-//}}}
-//{{{
-static GLint link_program (GLint vs, GLint fs) {
-
-  GLint prog = glCreateProgram();
-  glAttachShader (prog, vs);
-  glAttachShader (prog, fs);
-  glLinkProgram (prog);
-
-  GLint ok;
-  glGetProgramiv (prog, GL_LINK_STATUS, &ok);
-  if (!ok) {
-    /* Some drivers return a size of 1 for an empty log.  This is the size
-     * of a log that contains only a terminating NUL character. */
-    GLint size;
-    GLchar* info = NULL;
-    glGetProgramiv (prog, GL_INFO_LOG_LENGTH, &size);
-    if (size > 1) {
-      info = (GLchar*)malloc (size);
-      glGetProgramInfoLog (prog, size, NULL, info);
+      glGetShaderInfoLog (s, size, NULL, info);
+      throw std::runtime_error ("failed to compile shader: " + std::string(info) + "\nsource:\n" + std::string (source));
       }
 
-    throw std::runtime_error ("failed to link: " + std::string(info ? info : "<empty log>"));
+    return s;
     }
+  //}}}
+  //{{{
+  GLint link_program (GLint vs, GLint fs) {
 
-  return prog;
+    GLint prog = glCreateProgram();
+    glAttachShader (prog, vs);
+    glAttachShader (prog, fs);
+    glLinkProgram (prog);
+
+    GLint ok;
+    glGetProgramiv (prog, GL_LINK_STATUS, &ok);
+    if (!ok) {
+      /* Some drivers return a size of 1 for an empty log.  This is the size
+       * of a log that contains only a terminating NUL character. */
+      GLint size;
+      GLchar* info = NULL;
+      glGetProgramiv (prog, GL_INFO_LOG_LENGTH, &size);
+      if (size > 1) {
+        info = (GLchar*)malloc (size);
+        glGetProgramInfoLog (prog, size, NULL, info);
+        }
+
+      throw std::runtime_error ("failed to link: " + std::string(info ? info : "<empty log>"));
+      }
+
+    return prog;
+    }
+  //}}}
+  //{{{
+  void gl_setup (int width, int height, int window_width, int window_height) {
+
+    float w_factor = width / (float)window_width;
+    float h_factor = height / (float)window_height;
+    float max_dimension = std::max(w_factor, h_factor);
+
+    w_factor /= max_dimension;
+    h_factor /= max_dimension;
+
+    char vs[256];
+    snprintf (vs, sizeof(vs),
+              "attribute vec4 pos;\n"
+              "varying vec2 texcoord;\n"
+              "\n"
+              "void main() {\n"
+              "  gl_Position = pos;\n"
+              "  texcoord.x = pos.x / %f + 0.5;\n"
+              "  texcoord.y = 0.5 - pos.y / %f;\n"
+              "}\n",
+              2.0 * w_factor, 2.0 * h_factor);
+    vs[sizeof(vs) - 1] = 0;
+
+    GLint vs_s = compile_shader (GL_VERTEX_SHADER, vs);
+    const char* fs = "#extension GL_OES_EGL_image_external : enable\n"
+                     "precision mediump float;\n"
+                     "uniform samplerExternalOES s;\n"
+                     "varying vec2 texcoord;\n"
+                     "void main() {\n"
+                     "  gl_FragColor = texture2D(s, texcoord);\n"
+                     "}\n";
+
+    GLint fs_s = compile_shader (GL_FRAGMENT_SHADER, fs);
+    GLint prog = link_program (vs_s, fs_s);
+
+    glUseProgram (prog);
+
+    static const float verts[] = { -w_factor, -h_factor, w_factor, -h_factor, w_factor, h_factor, -w_factor, h_factor };
+    glVertexAttribPointer (0, 2, GL_FLOAT, GL_FALSE, 0, verts);
+    glEnableVertexAttribArray (0);
   }
-//}}}
+  //}}}
+  //{{{
+  void get_colour_space_info (std::optional<libcamera::ColorSpace> const& cs, EGLint &encoding, EGLint& range) {
+
+    encoding = EGL_ITU_REC601_EXT;
+    range = EGL_YUV_NARROW_RANGE_EXT;
+
+    if (cs == libcamera::ColorSpace::Sycc)
+      range = EGL_YUV_FULL_RANGE_EXT;
+    else if (cs == libcamera::ColorSpace::Smpte170m)
+      /* all good */;
+    else if (cs == libcamera::ColorSpace::Rec709)
+      encoding = EGL_ITU_REC709_EXT;
+    else
+      LOG(1, "EglPreview: unexpected colour space " << libcamera::ColorSpace::toString(cs));
+    }
+  //}}}
+  //{{{
+  void no_border (Display* display, Window window) {
+
+    static const unsigned MWM_HINTS_DECORATIONS = (1 << 1);
+    static const int PROP_MOTIF_WM_HINTS_ELEMENTS = 5;
+
+    typedef struct {
+      unsigned long flags;
+      unsigned long functions;
+      unsigned long decorations;
+      long inputMode;
+      unsigned long status;
+      } PropMotifWmHints;
+
+    PropMotifWmHints motif_hints;
+    Atom prop, proptype;
+    unsigned long flags = 0;
+
+    /* setup the property */
+    motif_hints.flags = MWM_HINTS_DECORATIONS;
+    motif_hints.decorations = flags;
+
+    /* get the atom for the property */
+    prop = XInternAtom (display, "_MOTIF_WM_HINTS", True);
+    if (!prop) {
+      /* something went wrong! */
+      return;
+      }
+
+    /* not sure this is correct, seems to work, XA_WM_HINTS didn't work */
+    proptype = prop;
+
+    XChangeProperty (display, window, /* display, window */
+                     prop, proptype, /* property, type */
+                     32, /* format: 32-bit datums */
+                     PropModeReplace, /* mode */
+                     (unsigned char *)&motif_hints, /* data */
+                     PROP_MOTIF_WM_HINTS_ELEMENTS /* nelements */
+                     );
+    }
+  //}}}
+  }
+
 //{{{
-static void gl_setup (int width, int height, int window_width, int window_height) {
-
-  float w_factor = width / (float)window_width;
-  float h_factor = height / (float)window_height;
-  float max_dimension = std::max(w_factor, h_factor);
-
-  w_factor /= max_dimension;
-  h_factor /= max_dimension;
-
-  char vs[256];
-  snprintf (vs, sizeof(vs),
-            "attribute vec4 pos;\n"
-            "varying vec2 texcoord;\n"
-            "\n"
-            "void main() {\n"
-            "  gl_Position = pos;\n"
-            "  texcoord.x = pos.x / %f + 0.5;\n"
-            "  texcoord.y = 0.5 - pos.y / %f;\n"
-            "}\n",
-            2.0 * w_factor, 2.0 * h_factor);
-  vs[sizeof(vs) - 1] = 0;
-
-  GLint vs_s = compile_shader (GL_VERTEX_SHADER, vs);
-  const char* fs = "#extension GL_OES_EGL_image_external : enable\n"
-                   "precision mediump float;\n"
-                   "uniform samplerExternalOES s;\n"
-                   "varying vec2 texcoord;\n"
-                   "void main() {\n"
-                   "  gl_FragColor = texture2D(s, texcoord);\n"
-                   "}\n";
-
-  GLint fs_s = compile_shader (GL_FRAGMENT_SHADER, fs);
-  GLint prog = link_program (vs_s, fs_s);
-
-  glUseProgram (prog);
-
-  static const float verts[] = { -w_factor, -h_factor, w_factor, -h_factor, w_factor, h_factor, -w_factor, h_factor };
-  glVertexAttribPointer (0, 2, GL_FLOAT, GL_FALSE, 0, verts);
-  glEnableVertexAttribArray (0);
-}
-//}}}
-
-//{{{
-EglPreview::EglPreview (Options const* options) : Preview(options), last_fd_(-1), first_time_(true) {
+EglPreview::EglPreview (Options const* options) 
+   : Preview(options), last_fd_(-1), first_time_(true) {
 
   display_ = XOpenDisplay (NULL);
   if (!display_)
@@ -213,56 +273,14 @@ EglPreview::EglPreview (Options const* options) : Preview(options), last_fd_(-1)
 EglPreview::~EglPreview() { }
 
 //{{{
-static void no_border (Display* display, Window window) {
+void EglPreview::makeWindow (char const* name) {
 
-  static const unsigned MWM_HINTS_DECORATIONS = (1 << 1);
-  static const int PROP_MOTIF_WM_HINTS_ELEMENTS = 5;
-
-  typedef struct {
-    unsigned long flags;
-    unsigned long functions;
-    unsigned long decorations;
-    long inputMode;
-    unsigned long status;
-    } PropMotifWmHints;
-
-  PropMotifWmHints motif_hints;
-  Atom prop, proptype;
-  unsigned long flags = 0;
-
-  /* setup the property */
-  motif_hints.flags = MWM_HINTS_DECORATIONS;
-  motif_hints.decorations = flags;
-
-  /* get the atom for the property */
-  prop = XInternAtom (display, "_MOTIF_WM_HINTS", True);
-  if (!prop) {
-    /* something went wrong! */
-    return;
-    }
-
-  /* not sure this is correct, seems to work, XA_WM_HINTS didn't work */
-  proptype = prop;
-
-  XChangeProperty (display, window, /* display, window */
-                   prop, proptype, /* property, type */
-                   32, /* format: 32-bit datums */
-                   PropModeReplace, /* mode */
-                   (unsigned char *)&motif_hints, /* data */
-                   PROP_MOTIF_WM_HINTS_ELEMENTS /* nelements */
-                   );
-  }
-//}}}
-
-//{{{
-void EglPreview::makeWindow (char const *name) {
-
-  int screen_num = DefaultScreen(display_);
+  int screen_num = DefaultScreen (display_);
   XSetWindowAttributes attr;
   unsigned long mask;
-  Window root = RootWindow(display_, screen_num);
-  int screen_width = DisplayWidth(display_, screen_num);
-  int screen_height = DisplayHeight(display_, screen_num);
+  Window root = RootWindow (display_, screen_num);
+  int screen_width = DisplayWidth (display_, screen_num);
+  int screen_height = DisplayHeight (display_, screen_num);
 
   // Default behaviour here is to use a 1024x768 window.
   if (width_ == 0 || height_ == 0) {
@@ -298,22 +316,20 @@ void EglPreview::makeWindow (char const *name) {
   int num_visuals;
   XVisualInfo *visinfo = XGetVisualInfo (display_, VisualIDMask, &visTemplate, &num_visuals);
 
-  /* window attributes */
+  // window attributes
   attr.background_pixel = 0;
   attr.border_pixel = 0;
   attr.colormap = XCreateColormap (display_, root, visinfo->visual, AllocNone);
   attr.event_mask = StructureNotifyMask | ExposureMask | KeyPressMask;
 
-  /* XXX this is a bad way to get a borderless window! */
+  // XXX this is a bad way to get a borderless window!
   mask = CWBackPixel | CWBorderPixel | CWColormap | CWEventMask;
-
-  window_ = XCreateWindow (display_, root, x_, y_, width_, height_, 0, visinfo->depth, 
+  window_ = XCreateWindow (display_, root, x_, y_, width_, height_, 0, visinfo->depth,
                            InputOutput, visinfo->visual, mask, &attr);
-
   if (options_->fullscreen)
     no_border (display_, window_);
 
-  /* set hints and properties */
+  // set hints and properties
   {
     XSizeHints sizehints;
     sizehints.x = x_;
@@ -327,10 +343,7 @@ void EglPreview::makeWindow (char const *name) {
 
   eglBindAPI (EGL_OPENGL_ES_API);
 
-  static const EGLint ctx_attribs[] = {
-    EGL_CONTEXT_CLIENT_VERSION, 2,
-    EGL_NONE
-    };
+  static const EGLint ctx_attribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
   egl_context_ = eglCreateContext (egl_display_, config, EGL_NO_CONTEXT, ctx_attribs);
   if (!egl_context_)
     throw std::runtime_error ("eglCreateContext failed");
@@ -356,22 +369,6 @@ void EglPreview::makeWindow (char const *name) {
 
   // This "undoes" the previous eglMakeCurrent.
   eglMakeCurrent (egl_display_, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-  }
-//}}}
-//{{{
-static void get_colour_space_info (std::optional<libcamera::ColorSpace> const &cs, EGLint &encoding, EGLint &range) {
-
-  encoding = EGL_ITU_REC601_EXT;
-  range = EGL_YUV_NARROW_RANGE_EXT;
-
-  if (cs == libcamera::ColorSpace::Sycc)
-    range = EGL_YUV_FULL_RANGE_EXT;
-  else if (cs == libcamera::ColorSpace::Smpte170m)
-    /* all good */;
-  else if (cs == libcamera::ColorSpace::Rec709)
-    encoding = EGL_ITU_REC709_EXT;
-  else
-    LOG(1, "EglPreview: unexpected colour space " << libcamera::ColorSpace::toString(cs));
   }
 //}}}
 
@@ -468,7 +465,7 @@ void EglPreview::Reset() {
 bool EglPreview::Quit() {
 
   XEvent event;
-  while (XCheckTypedWindowEvent (display_, window_, ClientMessage, &event)) 
+  while (XCheckTypedWindowEvent (display_, window_, ClientMessage, &event))
     if (static_cast<Atom>(event.xclient.data.l[0]) == wm_delete_window_)
       return true;
 
